8c8a22b63ffa1ab6c76a6bec803b502771503e03
Keks <keks@htmlacademy.ru>
2020-08-16 21:14:45 +0300

    :kissing_cat: Добавляет сборку Sass на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,51 @@
-const { src, dest, watch } = require("gulp");
-const handlebars = require("gulp-compile-handlebars");
-const rename = require("gulp-rename");
-const helpers = require("./helpers");
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
 
-const templatesFolder = "./templates";
+// Styles
 
-const paths = {
-  templates: `${templatesFolder}/*.handlebars`,
-  allTemplates: `${templatesFolder}/**/*.handlebars`,
-  components: `${templatesFolder}/components`,
-  dest: "./source",
-};
+const styles = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
+}
 
-const compileTemplates = () => {
-  const options = {
-    batch: [paths.components],
-    helpers,
-  };
+exports.styles = styles;
 
-  return src(paths.templates)
-    .pipe(handlebars({}, options))
-    .pipe(
-      rename({
-        extname: ".html",
-      })
-    )
-    .pipe(dest(paths.dest));
-};
+// Server
 
-const watchTemplates = () => watch(paths.allTemplates, compileTemplates);
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
 
-module.exports = {
-  default: compileTemplates,
-  watchTemplates,
-};
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
